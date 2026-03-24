9f64799c19036f009752748c4391238be443c6f7
Khushi Mehta <khushimehta82339@gmail.com>
2026-03-25 01:41:29 +0530

    initial backend commit

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,12 +1,12 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 
-// ✅ FINAL upload path (PUBLIC FOLDER)
-const uploadDir = "/var/www/manux/uploads";
+// make sure uploads folder exists
+const uploadDir = path.join(process.cwd(), "uploads");
 
-// make sure folder exists
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
+  fs.mkdirSync(uploadDir);
 }
 
 const storage = multer.diskStorage({
@@ -14,9 +14,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    // ✅ remove spaces (important)
-    const cleanName = file.originalname.replace(/\s+/g, "-");
-    cb(null, Date.now() + "-" + cleanName);
+    cb(null, Date.now() + "-" + file.originalname);
   }
 });
 
