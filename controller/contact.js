68c91dc99d1dbc76bb3c92e29646ffd1303bb79f
Khushi Mehta <khushimehta82339@gmail.com>
2026-03-29 23:00:56 +0530

    initial backend commit

--- controller/contact.js
+++ controller/contact.js
@@ -48,3 +48,22 @@ export const getContacts = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+
+
+
+export const deleteContact = async (req, res) => {
+  try {
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+
+    if (!contact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    res.json({ message: "Contact deleted successfully" });
+
+  } catch (error) {
+    console.log("DELETE ERROR:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createContact ,getContacts} from "../controller/contact.js";
+import { createContact ,getContacts,deleteContact} from "../controller/contact.js";
 import { protect, adminOnly } from "../middleware/auth.js";
 const router = express.Router();
 
 router.post("/", createContact);
 router.get("/", protect, adminOnly, getContacts);
-
+router.delete("/:id", protect, adminOnly, deleteContact);
 
 export default router;
